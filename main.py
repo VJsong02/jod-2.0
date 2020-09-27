f306f6825e26b1447160cc696fe1960094b7d495
VJsong02 <victor.song.02@gmail.com>
2020-09-27 18:48:53 +0200

    Added interest calculation

--- main.py
+++ main.py
@@ -14,19 +14,27 @@ loans = {
     datetime.datetime(2021,  2, 25): 7568,
     datetime.datetime(2021,  3, 25): 7568,
     datetime.datetime(2021,  4, 23): 7568,
-    datetime.datetime(2021,  5, 25): 3784
+    datetime.datetime(2021,  5, 25): 3784,
+    datetime.datetime(2021,  6, 25): 0,
+    datetime.datetime(2021,  7, 25): 0
 }
 
-def calc_debt():
+interests = {
+    2020: 1.0016,
+    2021: 1.0016
+}
+
+def calc_debt(now=datetime.datetime.now(), interest=interests):
     sum = 0
     for (date, amt) in loans.items():
-        if(date <= datetime.datetime.now()):
+        if(date <= now):
             sum += amt
+            sum *= (interest[now.year] ** (1 / 12))
     return sum
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('Logged in as {0.user}'.format(client))
 
 
 @client.event
@@ -35,10 +43,12 @@ async def on_message(message):
         return
 
     if message.content.startswith('$eval'):
+        print(message.content[5:])
         await message.channel.send(eval(message.content[5:]))
     
     if message.content == 'lån?':
-        await message.channel.send("Du har -" + str(calc_debt()) + " kr")
+        await message.channel.send("Du har -"\
+             + str(round(calc_debt(), 2)) + " kr")
 
 data = json.load(open('config.json',))
 client.run(data['token'])
